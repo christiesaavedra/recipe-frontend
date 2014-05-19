1f759ea6cfbcb2086515fbec637df47ce7ca3801
Christie <stephenson.mchristine@gmail.com>
2014-05-19 09:39:16 -0600

    Recipe Frontend

---
+++ public/js/filters.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/* Filters */
+
+angular.module('sproutApp.filters', [])
+    .filter('interpolate', ['version', function (version) {
+        return function (text) {
+            return String(text).replace(/\%VERSION\%/mg, version);
+        }
+    }])
+    .filter('favoritesFilter', function () {
+        return function(recipes, favoriteUser) {
+            var favorites = [];
+
+            if (favoriteUser == '') {
+                return recipes; // Do not filter.
+            }
+
+            for (var i = 0; i < recipes.length; i++) {
+                var recipe = recipes[i];
+
+                var favoriteIndex = recipe.favorited_by.indexOf(favoriteUser);
+
+                if (favoriteIndex >= 0) {
+                    favorites.push(recipe);
+                }
+            }
+
+            return favorites;
+        }
+    });
